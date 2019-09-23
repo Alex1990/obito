52200bc803cc9cd46318b02b049fd1d0114ac902
Alex Chao <alexchao1990@gmail.com>
2019-09-23 23:28:44 +0800

    Fix npm private package installation error #4

--- test/sync.spec.js
+++ test/sync.spec.js
@@ -33,6 +33,30 @@ describe('sync', () => {
     done()
   }, 30000)
 
+  test('should sync an npm private package to aliyun oss', async (done) => {
+    const client = new OSS(config.aliyun)
+    const pkg = '@babel/standalone@7.6.0'
+    const pkgMain = `${pkg}/babel.min.js`
+    const objectName = `${config.prefix}/${pkgMain}`
+
+    const ret = shell.exec(`${obitoBin} sync ${pkg}`)
+    if (ret.code !== 0) {
+      console.error(ret.stderr)
+    } else {
+      console.log(ret.stdout)
+    }
+    expect(ret.code).toBe(0)
+
+    const url = await client.signatureUrl(objectName)
+    const res = await rp({
+      url,
+      resolveWithFullResponse: true
+    })
+    expect(res.headers['content-type']).toBe('application/javascript')
+    expect(res.body).toMatch('babel')
+    done()
+  }, 30000)
+
   test('should sync an npm package to aws s3', async (done) => {
     const client = new S3({
       accessKeyId: config.s3.accessKeyId,
